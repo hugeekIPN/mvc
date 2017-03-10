51832bff424ddc3ff59a724b610a10044dee8870
hugeek <hugui_95@hotmail.com>
2017-03-10 14:54:43 -0600

    'proveedores_fix1.0'

--- views/proveedores.php
+++ views/proveedores.php
@@ -126,11 +126,50 @@
 					</div>
                      <div class="form-group ">
 					  <label class="control-label titulos" for="inputEstadoProveedores">Estado:</label>
-					  <input required type="text" class="form-control" id="inputEstadoProveedores">
-					</div>                                          
+                                                                                       
+                        <select name="state" class="form-control" id="inputEstadoProveedores">
+                        <option value="Aguascalientes">Aguascalientes</option>
+                        <option value="Baja California">Baja California</option>
+                        <option value="Baja California Sur">Baja California Sur</option>
+                        <option value="Campeche">Campeche</option>
+                        <option value="Chiapas">Chiapas</option>
+                        <option value="Chihuahua">Chihuahua</option>
+                        <option value="Coahuila">Coahuila</option>
+                        <option value="Colima">Colima</option>
+                        <option value="Ciudad de México" selected>Ciudad de México</option>
+                        <option value="Durango">Durango</option>
+                        <option value="Estado de México">Estado de México</option>
+                        <option value="Guanajuato">Guanajuato</option>
+                        <option value="Guerrero">Guerrero</option>
+                        <option value="Hidalgo">Hidalgo</option>
+                        <option value="Jalisco">Jalisco</option>
+                        <option value="Michoacán">Michoacán</option>
+                        <option value="Morelos">Morelos</option>
+                        <option value="Nayarit">Nayarit</option>
+                        <option value="Nuevo León">Nuevo León</option>
+                        <option value="Oaxaca">Oaxaca</option>
+                        <option value="Puebla">Puebla</option>
+                        <option value="Querétaro">Querétaro</option>
+                        <option value="Quintana Roo">Quintana Roo</option>
+                        <option value="San Luis Potosí">San Luis Potosí</option>
+                        <option value="Sinaloa">Sinaloa</option>
+                        <option value="Sonora">Sonora</option>
+                        <option value="Tabasco">Tabasco</option>
+                        <option value="Tamaulipas">Tamaulipas</option>
+                        <option value="Tlaxcala">Tlaxcala</option>
+                        <option value="Veracruz">Veracruz</option>
+                        <option value="Yucatán">Yucatán</option>
+                        <option value="Zacatecas">Zacatecas</option>
+                        </select>
+                    </div>
+                                                                                                
+                                                                                                  
 					<div class="form-group">
 					  <label class="control-label titulos" for="inputPaisProveedores">País:</label>
-					  <input required type="text" class="form-control" id="inputPaisProveedores">
+					  <select class="form-control" id="inputPaisProveedores"  onchange="Proveedores.pais();">
+                            <option value="México">México</option>
+                            <option value="EUA">EUA</option>
+                        </select>
 					</div>
 					<div class="form-group ">
 					  <label class="control-label titulos" for="inputCodigoPostalProveedores">Código Postal:</label>
